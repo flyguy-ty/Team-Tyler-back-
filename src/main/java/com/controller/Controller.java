9414bd18dffaa20094fec17c789076b908c4d552
Walter Darby <walterdarbystcilien@gmail.com>
2022-03-18 13:20:54 -0400

    init commit

--- src/main/java/com/controller/Controller.java
+++ src/main/java/com/controller/Controller.java
@@ -6,7 +6,7 @@ import com.model.Author;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-public class Controller {
+public class  Controller {
 //    @Autowired
 //    UserService userService;
 
